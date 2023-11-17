fe8859cf64d93169bfff10aa35b0936cd9bc2ec0
Anna <anjanadvorna@gmail.com>
2023-11-17 18:28:48 +0200

    Update ContactForm.jsx

--- src/components/PhoneBook/ContactForm/ContactForm.jsx
+++ src/components/PhoneBook/ContactForm/ContactForm.jsx
@@ -47,7 +47,7 @@ class ContactForm extends Component {
                             value={name}
                             onChange={this.changeInput}
                             placeholder="Name Surname"
-                            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                            pattern="^[a-zA-Zа-яА-Я-іІ-їЇ-Йй-єЄ]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                             title="Name may contain only letters, apostrophe, dash and spaces."
                             required
                         />
@@ -62,7 +62,7 @@ class ContactForm extends Component {
                             name="number"
                             value={number}
                             onChange={this.changeInput}
-                            placeholder="xxx-xxx-xx-xx"
+                            placeholder="+хх (xxx) xxx-xx-xx"
                             pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
                             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                             required
